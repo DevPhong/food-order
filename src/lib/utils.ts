5ec859d750d2d7839077dcdab641b4c5e203ec65
DevPhong <phonghoangle96@gmail.com>
2025-07-02 08:53:37 +0700

    feat/620: Tạo http client để gọi API

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+// Xóa dấu gạch chéo đầu chuỗi
+// Ví dụ: "/path/to/resource" -> "path/to/resource"
+export const normalizePath = (path: string) => {
+  return path.startsWith("/") ? path.slice(1) : path;
+};
