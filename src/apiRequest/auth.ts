f3b2c79edc512c678784ae63240c6f36f7d07ab0
DevPhong <phonghoangle96@gmail.com>
2025-07-03 08:25:28 +0700

    feat/621 - Tạo Login Route Handler

---
+++ src/apiRequest/auth.ts
@@ -0,0 +1,12 @@
+import http from "@/lib/http";
+import { LoginBodyType, LoginResType } from "@/schemaValidations/auth.schema";
+
+const authApiRequest = {
+  sLogin: (body: LoginBodyType) => http.post<LoginResType>("/auth/login", body), // server side
+  login: (body: LoginBodyType) =>
+    http.post<LoginResType>("api/auth/login", body, {
+      baseUrl: "",
+    }), // client side
+};
+
+export default authApiRequest;
