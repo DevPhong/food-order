69496ca54347a3d0565641ed937b1d23fe2343d5
DevPhong <phonghoangle96@gmail.com>
2025-07-11 10:08:36 +0700

    feat/627 - Code route handler logout

--- src/apiRequest/auth.ts
+++ src/apiRequest/auth.ts
@@ -1,10 +1,36 @@
 import http from "@/lib/http";
-import { LoginBodyType, LoginResType } from "@/schemaValidations/auth.schema";
+import {
+  LoginBodyType,
+  LoginResType,
+  LogoutBodyType,
+} from "@/schemaValidations/auth.schema";
 
 const authApiRequest = {
   sLogin: (body: LoginBodyType) => http.post<LoginResType>("auth/login", body), // server next side call
   login: (body: LoginBodyType) =>
     http.post<LoginResType>("api/auth/login", body, {
+      baseUrl: "",
+    }), // client call to API side
+
+  sLogout: (
+    body: LogoutBodyType & {
+      accessToken: string;
+    }
+  ) =>
+    http.post(
+      "auth/logout",
+      {
+        refreshToken: body.refreshToken,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${body.accessToken}`,
+        },
+      }
+    ), // server next side call
+
+  logout: () =>
+    http.post("api/auth/logout", null, {
       baseUrl: "",
     }), // client call to API side
 };
