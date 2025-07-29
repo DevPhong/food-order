2630b0502f2e7790d5c1f898757cdb718ed18bfc
DevPhong <phonghoangle96@gmail.com>
2025-07-29 10:00:38 +0700

    feat/835 - Tự động logout khi access token hết hạn (trường hợp access token bị xóa khỏi cookie)

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,16 +6,29 @@ const unAuthPaths = ["/login"];
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  const isAuth = Boolean(request.cookies.get("accessToken")?.value);
+  const accessToken = request.cookies.get("accessToken")?.value;
+  const refreshToken = request.cookies.get("refreshToken")?.value;
 
-  // Nếu chưa đăng nhập thì sẽ redirect về login
-  if (privatePaths.some((path) => pathname.startsWith(path)) && !isAuth) {
-    return NextResponse.redirect(new URL("/login", request.url));
+  // Nếu chưa đăng nhập thì không cho private paths
+  if (privatePaths.some((path) => pathname.startsWith(path)) && !accessToken) {
+    const url = new URL("/login", request.url);
+    return NextResponse.redirect(url);
   }
 
   // Đăng nhập rồi thì không vào login
-  if (unAuthPaths.some((path) => pathname.startsWith(path)) && isAuth) {
+  if (unAuthPaths.some((path) => pathname.startsWith(path)) && accessToken) {
     return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  // Trường hợp đăng nhập rồi nhưng accessToken hết hạn
+  if (
+    privatePaths.some((path) => pathname.startsWith(path)) &&
+    !accessToken &&
+    refreshToken
+  ) {
+    const url = new URL("/logout", request.url);
+    url.searchParams.set("refreshToken", refreshToken);
+    return NextResponse.redirect(url);
   }
   return NextResponse.next();
 }
